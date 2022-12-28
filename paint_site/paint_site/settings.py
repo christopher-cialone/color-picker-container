cc64d681d4396c465e16016a2ffa0dd198aa2dd0
Christopher Cialone <ccialone0929@gmail.com>
2022-12-28 18:52:19 -0500

    Registered app in settings file

--- paint_site/paint_site/settings.py
+++ paint_site/paint_site/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'paint_app.apps.PaintAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
